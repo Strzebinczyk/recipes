a0771caf0f6d7b924a8a294ea256d0050e801cd2
Strzebinczyk <kar.strzebinczyk@gmail.com>
2025-03-24 20:56:08 +0100

    attempt to solve github actions problem

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -47,6 +47,10 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   config.include Devise::Test::IntegrationHelpers, type: :request
 
+  config.before(:each, type: :system) do
+    driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+  end
+
   config.before(:each, type: :request) do
     Rails.application.reload_routes_unless_loaded
   end
