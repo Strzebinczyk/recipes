b97abcec498bd87068f68780bd0b50b8ad7f6869
Strzebinczyk <kar.strzebinczyk@gmail.com>
2025-04-14 14:50:31 +0200

    Fix adding image to new recipe

---
+++ app/javascript/controllers/delete-image-controller.js
@@ -0,0 +1,10 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  delete(e) {
+    e.preventDefault();
+
+    this.element.querySelector("check_box").value = "1"
+    this.element.classList.toggle("hidden")
+  }
+}
