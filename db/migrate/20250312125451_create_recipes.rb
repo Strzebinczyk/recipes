a6e1b010088b888254648f1239055cd4c583e510
Strzebinczyk <kar.strzebinczyk@gmail.com>
2025-04-15 11:00:24 +0200

    start working on ingredients'

--- db/migrate/20250312125451_create_recipes.rb
+++ db/migrate/20250312125451_create_recipes.rb
@@ -6,7 +6,6 @@ class CreateRecipes < ActiveRecord::Migration[8.0]
       t.belongs_to :user, index: true, foreign_key: true
       t.string :name
       t.integer :serving
-      t.text :ingredients
 
       t.timestamps
     end
