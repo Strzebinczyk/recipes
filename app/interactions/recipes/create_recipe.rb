3096dc5131a30fc5dee0857f7bd62d30143808ca
Strzebinczyk <kar.strzebinczyk@gmail.com>
2025-04-24 19:34:26 +0200

    fix github CI tests

--- app/interactions/recipes/create_recipe.rb
+++ app/interactions/recipes/create_recipe.rb
@@ -2,32 +2,34 @@
 
 require_relative '../../models/ingredient'
 
-class CreateRecipe < ActiveInteraction::Base
-  object :user
-  hash :params, strip: false
+module Recipes
+  class CreateRecipe < ActiveInteraction::Base
+    object :user
+    hash :params, strip: false
 
-  def execute # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
-    i = 0
-    new_params = params
+    def execute # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
+      i = 0
+      new_params = params
 
-    params['recipe_ingredients_attributes'].length.times do
-      name = params['recipe_ingredients_attributes'].values[i]['name']
-      quantity = params['recipe_ingredients_attributes'].values[i]['quantity']
-      next if name == '' || quantity == ''
+      params['recipe_ingredients_attributes'].length.times do
+        name = params['recipe_ingredients_attributes'].values[i]['name']
+        quantity = params['recipe_ingredients_attributes'].values[i]['quantity']
+        next if name == '' || quantity == ''
 
-      new_params['recipe_ingredients_attributes'].values[i].delete('name')
-      ingredient = Ingredient.all.select { |ingredient| ingredient.name == name }[0]
-
-      unless ingredient
-        ingredient = Ingredient.build(name: name)
-        ingredient.save
+        new_params['recipe_ingredients_attributes'].values[i].delete('name')
         ingredient = Ingredient.all.select { |ingredient| ingredient.name == name }[0]
+
+        unless ingredient
+          ingredient = Ingredient.build(name: name)
+          ingredient.save
+          ingredient = Ingredient.all.select { |ingredient| ingredient.name == name }[0]
+        end
+
+        new_params['recipe_ingredients_attributes'].values[i]['ingredient_id'] = ingredient.id
+
+        i += 1
       end
-
-      new_params['recipe_ingredients_attributes'].values[i]['ingredient_id'] = ingredient.id
-
-      i += 1
+      user.recipes.build(new_params)
     end
-    user.recipes.build(new_params)
   end
 end
--- app/interactions/recipes/update_recipe.rb
+++ app/interactions/recipes/update_recipe.rb
@@ -4,37 +4,39 @@ require_relative '../../models/ingredient'
 require_relative '../../models/recipe'
 require_relative '../../controllers/recipes_controller'
 
-class UpdateRecipe < ActiveInteraction::Base
-  hash :params, strip: false
-  string :id
+module Recipes
+  class UpdateRecipe < ActiveInteraction::Base
+    hash :params, strip: false
+    string :id
 
-  def execute # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
-    i = 0
-    new_params = params
+    def execute # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
+      i = 0
+      new_params = params
 
-    return new_params unless params['recipe_ingredients_attributes']
+      return new_params unless params['recipe_ingredients_attributes']
 
-    params['recipe_ingredients_attributes'].length.times do
-      name = params['recipe_ingredients_attributes'].values[i]['name']
-      quantity = params['recipe_ingredients_attributes'].values[i]['quantity']
+      params['recipe_ingredients_attributes'].length.times do
+        name = params['recipe_ingredients_attributes'].values[i]['name']
+        quantity = params['recipe_ingredients_attributes'].values[i]['quantity']
 
-      if name == '' || quantity == ''
-        new_params['recipe_ingredients_attributes'].delete(params['recipe_ingredients_attributes'].keys[i])
-        next
-      end
+        if name == '' || quantity == ''
+          new_params['recipe_ingredients_attributes'].delete(params['recipe_ingredients_attributes'].keys[i])
+          next
+        end
 
-      new_params['recipe_ingredients_attributes'].values[i].delete('name')
-      ingredient = Ingredient.all.select { |ingr| ingr.name == name }[0]
-
-      unless ingredient
-        ingredient = Ingredient.build(name: name)
-        ingredient.save
+        new_params['recipe_ingredients_attributes'].values[i].delete('name')
         ingredient = Ingredient.all.select { |ingr| ingr.name == name }[0]
-      end
-      new_params['recipe_ingredients_attributes'].values[i]['ingredient_id'] = ingredient.id
 
-      i += 1
+        unless ingredient
+          ingredient = Ingredient.build(name: name)
+          ingredient.save
+          ingredient = Ingredient.all.select { |ingr| ingr.name == name }[0]
+        end
+        new_params['recipe_ingredients_attributes'].values[i]['ingredient_id'] = ingredient.id
+
+        i += 1
+      end
+      new_params
     end
-    new_params
   end
 end
