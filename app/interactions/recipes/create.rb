973e10977f3f225a3dd3ac7a77d884c8bd67e21a
Strzebinczyk <kar.strzebinczyk@gmail.com>
2025-04-25 13:20:50 +0200

    refactor

--- app/interactions/recipes/create.rb
+++ app/interactions/recipes/create.rb
@@ -6,25 +6,19 @@ module Recipes
     hash :params, strip: false
 
     def execute # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
-      i = 0
-      new_recipe_ingredients_attributes = {}
+      new_recipe_ingredients_attributes =
+        params['recipe_ingredients_attributes'].transform_values do |recipe_ingredient|
+          name = recipe_ingredient['name']
+          recipe_ingredient.delete('name')
 
-      params['recipe_ingredients_attributes'].each do
-        name = params['recipe_ingredients_attributes'].values[i]['name']
-        quantity = params['recipe_ingredients_attributes'].values[i]['quantity']
-        next if name == '' || quantity == ''
+          ingredient = Ingredient.find_by(name: name)
+          ingredient ||= Ingredient.create(name: name)
 
-        new_recipe_ingredients_attributes[params['recipe_ingredients_attributes'].keys[i]] =
-          params['recipe_ingredients_attributes'].values[i]
-        new_recipe_ingredients_attributes.values[i].delete('name')
-
-        ingredient = Ingredient.find_by(name: name)
-        ingredient ||= Ingredient.create(name: name)
-
-        new_recipe_ingredients_attributes.values[i]['ingredient_id'] = ingredient.id
-
-        i += 1
-      end
+          recipe_ingredient['ingredient_id'] = ingredient.id
+          recipe_ingredient
+        end.reject do |recipe_ingredient| # rubocop:disable Style/MultilineBlockChain
+          recipe_ingredient['name'] == '' || recipe_ingredient['quantity'] == ''
+        end
       new_params = { recipe_ingredients_attributes: new_recipe_ingredients_attributes, **params }
       user.recipes.build(new_params)
     end
--- app/interactions/recipes/update.rb
+++ app/interactions/recipes/update.rb
@@ -6,27 +6,21 @@ module Recipes
     object :recipe
 
     def execute # rubocop:disable Metrics/AbcSize,Metrics/MethodLength,Metrics/CyclomaticComplexity,Metrics/PerceivedComplexity
-      i = 0
-      new_recipe_ingredients_attributes = {}
-
       if params['recipe_ingredients_attributes']
+        new_recipe_ingredients_attributes =
+          params['recipe_ingredients_attributes'].transform_values do |recipe_ingredient|
+            name = recipe_ingredient['name']
+            recipe_ingredient.delete('name')
 
-        params['recipe_ingredients_attributes'].each do
-          name = params['recipe_ingredients_attributes'].values[i]['name']
-          quantity = params['recipe_ingredients_attributes'].values[i]['quantity']
+            ingredient = Ingredient.find_by(name: name)
+            ingredient ||= Ingredient.create(name: name)
 
-          next if name == '' || quantity == ''
+            recipe_ingredient['ingredient_id'] = ingredient.id
+            recipe_ingredient
+          end.reject do |recipe_ingredient| # rubocop:disable Style/MultilineBlockChain
+            recipe_ingredient['name'] == '' || recipe_ingredient['quantity'] == ''
+          end
 
-          new_recipe_ingredients_attributes[params['recipe_ingredients_attributes'].keys[i]] =
-            params['recipe_ingredients_attributes'].values[i]
-          new_recipe_ingredients_attributes.values[i].delete('name')
-
-          ingredient = Ingredient.find_by(name: name)
-          ingredient ||= Ingredient.create(name: name)
-          new_recipe_ingredients_attributes.values[i]['ingredient_id'] = ingredient.id
-
-          i += 1
-        end
         new_params = { recipe_ingredients_attributes: new_recipe_ingredients_attributes, **params }
         errors.merge!(recipe.errors) unless recipe.update(new_params)
       else
