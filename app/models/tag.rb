1c397d6cf39e153e5b416cb7e709cd15016cdb13
Strzebinczyk <kar.strzebinczyk@gmail.com>
2025-05-08 11:44:44 +0200

    formatting

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,8 +3,4 @@
 class Tag < ApplicationRecord
   has_many :taggings, dependent: :destroy
   has_many :recipes, through: :taggings
-
-  def self.ransackable_attributes(_auth_object = nil)
-    ['name']
-  end
 end
