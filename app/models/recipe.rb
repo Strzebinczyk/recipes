b6cfb1c9707193e26b5752625efe759623765952
Strzebinczyk <kar.strzebinczyk@gmail.com>
2025-05-09 14:40:48 +0200

    Translation of image errors

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -35,11 +35,11 @@ class Recipe < ApplicationRecord
   def acceptable_image
     return unless image.attached?
 
-    errors.add(:image, 'is too big') unless image.blob.byte_size <= 1.megabyte
+    errors.add(:image, 'jest za duży') unless image.blob.byte_size <= 1.megabyte
 
     acceptable_types = ['image/jpeg', 'image/png']
     return if acceptable_types.include?(image.content_type)
 
-    errors.add(:image, 'must be a JPEG or PNG')
+    errors.add(:image, 'musi być formatu JPEG lub PNG')
   end
 end
