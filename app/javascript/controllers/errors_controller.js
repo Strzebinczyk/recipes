636a9be40c260144dd5fc9877ab089cdfea01fdf
Strzebinczyk <kar.strzebinczyk@gmail.com>
2025-05-09 14:31:07 +0200

    refactor

--- app/javascript/controllers/errors_controller.js
+++ app/javascript/controllers/errors_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    let field = this.element.previousElementSibling.children[0]
+    const field = this.element.previousElementSibling.children[0]
     field.classList.add("is-invalid")
   }
 }
