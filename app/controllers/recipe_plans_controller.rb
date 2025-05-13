9d8ce03d079927fc3740739b3cff35948f5a1244
Strzebinczyk <kar.strzebinczyk@gmail.com>
2025-05-13 14:31:34 +0200

    Add recipe to plan

--- app/controllers/recipe_plans_controller.rb
+++ app/controllers/recipe_plans_controller.rb
@@ -1,6 +1,17 @@
 # frozen_string_literal: true
 
 class RecipePlansController < ApplicationController
-  def new
+  def create
+    @recipe = Recipe.find_by(id: params[:recipe])
+    @plan = Plan.find_by(id: params[:plan])
+    @recipe_plan = RecipePlan.build(plan_id: params[:plan], recipe_id: params[:recipe])
+
+    respond_to do |format|
+      if @recipe_plan.save
+        format.html { redirect_back fallback_location: root_path, notice: 'RecipePlan was successfully created.' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
   end
 end
--- app/models/plan.rb
+++ app/models/plan.rb
@@ -3,8 +3,8 @@
 class Plan < ApplicationRecord
   validates :name, presence: true
 
-  has_many :recipes, through: :recipe_plans
   has_many :recipe_plans, dependent: :destroy
+  has_many :recipes, through: :recipe_plans
   belongs_to :user
 
   accepts_nested_attributes_for :recipe_plans, allow_destroy: true, reject_if: :all_blank
