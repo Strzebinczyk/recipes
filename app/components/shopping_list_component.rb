b297427ab14fdc526bf292b85320d81d5eadbb15
Strzebinczyk <kar.strzebinczyk@gmail.com>
2025-05-22 12:30:57 +0200

    refactor shopping list component

--- app/components/shopping_list_component.rb
+++ app/components/shopping_list_component.rb
@@ -9,15 +9,6 @@ class ShoppingListComponent < ViewComponent::Base
     @ingredients_hash = create_ingredients_hash(shopping_list_ingredients_hash)
   end
 
-  def unit_declention(unit, amount)
-    allowed_misc_units = %w[łyżcz łyż szt ząb pusz pęcz szkl garść szczypt kawał opak]
-
-    return unit if unit.in? ['g', 'do smaku', 'ml']
-    return nil if unit.nil?
-
-    I18n.t("units.#{unit}", count: amount) if allowed_misc_units.include?(unit)
-  end
-
   def create_shopping_list_ingredient_hash(shopping_list)
     shopping_list.shopping_list_ingredients.each_with_object({}) do |ingredient_item, hash|
       ingredient = ingredient_item.ingredient
@@ -27,26 +18,29 @@ class ShoppingListComponent < ViewComponent::Base
   end
 
   def create_ingredients_hash(shopping_list_ingredients_hash) # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
-    ingredients_hash = {}
-
-    shopping_list_ingredients_hash.each do |ingredient, shopping_list_ingridient_array|
-      quantities = {}
-      quantities_readable = {}
-
-      shopping_list_ingridient_array.each do |shopping_list_ingredient|
-        quantities[shopping_list_ingredient.quantity_unit] = (quantities[shopping_list_ingredient.quantity_unit] || 0) +
+    shopping_list_ingredients_hash.each_with_object({}) do |(ingredient, shopping_list_ingridient_array), hash|
+      quantities = shopping_list_ingridient_array.each_with_object({}) do |shopping_list_ingredient, quantities|
+        quantities[shopping_list_ingredient.quantity_unit] ||= 0
+        quantities[shopping_list_ingredient.quantity_unit] = quantities[shopping_list_ingredient.quantity_unit] +
                                                              shopping_list_ingredient.quantity_amount
       end
 
-      quantities.each do |unit, amount|
+      quantities_readable = quantities.each_with_object({}) do |(unit, amount), quantities_readable|
         unit = unit_declention(unit, amount)
         amount = amount.to_i if (amount % 1).zero?
         quantities_readable[unit] = amount
       end
 
-      ingredients_hash[ingredient.name] = quantities_readable
+      hash[ingredient.name] = quantities_readable
     end
+  end
 
-    ingredients_hash
+  def unit_declention(unit, amount)
+    allowed_misc_units = %w[łyżcz łyż szt ząb pusz pęcz szkl garść szczypt kawał opak]
+
+    return unit if unit.in? ['g', 'do smaku', 'ml']
+    return nil if unit.nil?
+
+    I18n.t("units.#{unit}", count: amount) if allowed_misc_units.include?(unit)
   end
 end
